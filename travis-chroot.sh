d606478a7d39b1055ee33f371f8709af31144cc0
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-07-30 18:19:43 -0400

    Try native instead.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis-chroot.sh
+++ travis-chroot.sh
@@ -6,10 +6,15 @@ cd /srv/work
 
 . ./travis-common.sh
 
-apt-get install -y --force-yes software-properties-common # for add-apt-repository
-add-apt-repository -y ppa:hvr/ghc
-apt-get update
-apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+#   apt-get install -y --force-yes software-properties-common # for add-apt-repository
+#   add-apt-repository -y ppa:hvr/ghc
+#   apt-get update
+#   apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+apt-get install -y --force-yes ghc cabal-install happy alex
+ghc --version
+cabal --version
 cabal update
 
-./travis-build.sh
+(cd cabal/Cabal && cabal install)
+(cd cabal/cabal-install && cabal install)
+gzip $HOME/.cabal/bin/cabal -c > cabal-install-$VERSION-$ARCH-$OS.gz
