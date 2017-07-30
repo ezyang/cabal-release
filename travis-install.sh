099c1c3beb83bf66d37b80a4bc26431d9f88401b
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-07-30 01:19:15 -0400

    happy alex
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis-install.sh
+++ travis-install.sh
@@ -4,7 +4,7 @@ set -ex
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     sudo add-apt-repository -y ppa:hvr/ghc
     sudo apt-get update
-    sudo apt-get install --force-yes ghc-$GHCVER:$UARCH cabal-install-$BOOTVER:$UARCH
+    sudo apt-get install --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     curl -OL http://downloads.haskell.org/~ghc/$GHCVER/ghc-${GHCVER}-x86_64-apple-darwin.tar.xz
     tar -xJf ghc-*.tar.xz
@@ -16,6 +16,8 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     mkdir "${HOME}/bin"
     curl -L http://web.mit.edu/~ezyang/Public/cabal-install-2.0.0.0-osx.gz | gunzip -c > "${HOME}/bin/cabal"
     chmod a+x "${HOME}/bin/cabal"
+
+    cabal install -j2 happy alex
 else
     echo "Not linux or osx: $TRAVIS_OS_NAME"
     false
