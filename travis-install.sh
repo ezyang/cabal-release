a3e8960e995847f8b0e6a7bf57b9081aa6b1de26
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-07-30 01:24:08 -0400

    chroot
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis-install.sh
+++ travis-install.sh
@@ -2,9 +2,21 @@
 set -ex
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo add-apt-repository -y ppa:hvr/ghc
-    sudo apt-get update
-    sudo apt-get install --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+    if [ "$ARCH" = "x86_64" ]; then
+        sudo add-apt-repository -y ppa:hvr/ghc
+        sudo apt-get update
+        sudo apt-get install --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+    else
+        sudo apt-get install --force-yes debootstrap schroot
+        sudo cp trusty_i386.conf /etc/schroot/chroot.d/trusty_i386.conf
+        sudo mkdir -p /srv/chroot/trusty_i386
+        sudo debootstrap --variant=buildd --arch=i386 trusty /srv/chroot/trusty_i386 http://archive.ubuntu.com/ubuntu/
+        schroot -l
+        schroot -c trusty_i386 -u root
+        sudo add-apt-repository -y ppa:hvr/ghc
+        sudo apt-get update
+        sudo apt-get install --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+    fi
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     curl -OL http://downloads.haskell.org/~ghc/$GHCVER/ghc-${GHCVER}-x86_64-apple-darwin.tar.xz
     tar -xJf ghc-*.tar.xz
