f689381172459a81d3f2aeed51b3c20ca928a452
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-07-30 19:41:47 -0400

    Install more things.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis-chroot.sh
+++ travis-chroot.sh
@@ -11,7 +11,8 @@ cd /srv/work
 #   apt-get update
 #   apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
 apt-get update
-apt-get install -y --force-yes ghc cabal-install happy alex libz-dev
+# attempted workaround from https://github.com/haskell/cabal/issues/3474
+apt-get install -y --force-yes ghc cabal-install happy alex libz-dev c2hs cpphs hscolour
 ghc --version
 cabal --version
 cabal update
