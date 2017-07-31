ae74a49c645c9e95fe5ac67561cb2f5db2e660e7
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-07-30 22:46:07 -0400

    libz
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis-chroot.sh
+++ travis-chroot.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get install -y --force-yes software-properties-common # for add-apt-repository
 add-apt-repository -y ppa:hvr/ghc
 apt-get update
-apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7 libz-dev
 
 # apt-get update
 # attempted workaround from https://github.com/haskell/cabal/issues/3474
