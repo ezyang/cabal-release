96e452fd4cf2539136b9c9c958b4b6e6d1a93b18
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-07-30 20:09:07 -0400

    Try dis
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis-chroot.sh
+++ travis-chroot.sh
@@ -6,13 +6,14 @@ cd /srv/work
 
 . ./travis-common.sh
 
-#   apt-get install -y --force-yes software-properties-common # for add-apt-repository
-#   add-apt-repository -y ppa:hvr/ghc
-#   apt-get update
-#   apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+apt-get install -y --force-yes software-properties-common # for add-apt-repository
+add-apt-repository -y ppa:hvr/ghc
 apt-get update
+apt-get install -y --force-yes ghc-$GHCVER cabal-install-$BOOTVER happy-1.19.5 alex-3.1.7
+
+# apt-get update
 # attempted workaround from https://github.com/haskell/cabal/issues/3474
-apt-get install -y --force-yes ghc cabal-install happy alex libz-dev c2hs cpphs hscolour hugs
+# apt-get install -y --force-yes ghc cabal-install happy alex libz-dev c2hs cpphs hscolour hugs
 ghc --version
 cabal --version
 cabal update
